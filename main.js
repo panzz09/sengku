77493ff414f37086fbf7c53b5d667f897ad12e98
Muhammad Irvan <mirvan3107@gmail.com>
2024-12-02 12:06:08 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+function clicks() {
+  $("#div1").hide();
+  $("#div2").show();
+  $("#div2").addClass("fadeinv2");
+
+  document.getElementById("bg-music").play();
+  var typed = new Typed(".katakata", {
+    strings: [
+      "hii sayangku shellaa",
+      "i just wanna say",
+      "everyday kalo sama kamu bawaannya happy banget",
+      "i just wanna say thank you for everything",
+      "terima kasih udah selalu ada",
+      "tiap hari call an",
+      "tapi sayangnya jauh huhuu",
+      "sedih yaa tapi gapapa",
+      "aku cuma mau ingat in aja",
+      "1. Jangan aneh aneh",
+      "2. Kabarin terus kemanapun dimanapun",
+      "3. Inget aku yaa sayaang xixixixixi",
+      "4. Jangan pernah ngerasa kamu sendirian, kamu punya aku, ceritaa apapun ituu sama aku yakk",
+      "udah itu aja yak",
+      "semoga lancar hehehe",
+
+      "lopyu shella",
+      "Kamu Tidak Sendirian",
+    ],
+    typeSpeed: 80,
+  });
+}
